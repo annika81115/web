a31d06d1d3f8426bdc3f575fda6f5ed50159eb2b
Annikas Mac <ammfsee@gmail.com>
2024-04-04 01:27:50 +0200

    add the missing files to commit

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Navbar from "$lib/components/Navbar.svelte"
+</script>
+
+<Navbar />
+<form action="mailto:recipient@example.com" method="get" enctype="text/plain">
+    Subject:<br>
+    <input type="text" name="subject" placeholder="Your Name"><br>
+    Email:<br>
+    <input type="email" name="email" placeholder="Your Email"><br>
+    Message:<br>
+    <textarea name="body" rows="5" cols="30" placeholder="Your Message"></textarea><br>
+    <input type="submit" value="Send">
+</form>
